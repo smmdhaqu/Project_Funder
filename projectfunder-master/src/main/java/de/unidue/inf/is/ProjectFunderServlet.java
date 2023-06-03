f439ebc59a3fc5a30e397940dcd4f15536fb589c
Shamsul Haque <74871535+smmdhaqu@users.noreply.github.com>
2023-06-03 12:53:08 +0200

    Add files via upload

---
+++ projectfunder-master/src/main/java/de/unidue/inf/is/ProjectFunderServlet.java
@@ -0,0 +1,38 @@
+package de.unidue.inf.is;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import de.unidue.inf.is.utils.DBUtil;
+
+
+
+/**
+ * Das könnte die Eintrittsseite sein.
+ */
+public final class ProjectFunderServlet extends HttpServlet {
+
+    private static final long serialVersionUID = 1L;
+
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+                    throws ServletException, IOException {
+  
+        boolean databaseExists = DBUtil.checkDatabaseExistsExternal();
+
+        if (databaseExists) {
+            request.setAttribute("db2exists", "vorhanden! Supi!");
+        }
+        else {
+            request.setAttribute("db2exists", "nicht vorhanden :-(");
+        }
+
+        request.getRequestDispatcher("projectFunder_start.ftl").forward(request, response);
+    }
+
+}
